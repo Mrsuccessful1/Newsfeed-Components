b4e96155484960a20bc63c6adbfd9c885fc58e8a
Mrsuccessful1 <mrsuccessfuldev@gmail.com>
2019-03-13 16:44:06 -0500

    articles

--- Article/Article.js
+++ Article/Article.js
@@ -9,12 +9,12 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expand = this.expandButton;
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    expandButton.addEventListener('click',() => this.expandArticle());
+    this.expandButton.addEventListener('click',() => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    
+    classList.toggle('.article')
   }
 }
 
@@ -25,8 +25,8 @@ class Article {
 - With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
 
 */
-
 let articles = document.querySelectorAll(".article");
-articles.forEach(function(article){
+
+articles.forEach(function('article'){
 return new Articles(article);
 });
