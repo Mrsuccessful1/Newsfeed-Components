5adac064a324e2f1b9113f5902859b7005f2959b
Mrsuccessful1 <mrsuccessfuldev@gmail.com>
2019-03-13 23:52:13 -0500

    confused, am I supposed to create a button content div to toggle

--- Article/Article.js
+++ Article/Article.js
@@ -7,16 +7,16 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expand = this.expandButton;
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click',() => this.expandArticle());
-  }
+  };
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    classList.toggle('.article')
+    this.expandButton.classList.toggle('')
   }
-}
+};
 
 /* START HERE: 
 
@@ -27,6 +27,6 @@ class Article {
 */
 let articles = document.querySelectorAll(".article");
 
-articles.forEach(function('article'){
-return new Articles(article);
+articles.forEach(function(article){
+  return  new Article(article);
 });
